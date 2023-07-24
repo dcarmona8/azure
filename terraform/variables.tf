bbc611bf197f6252ec27bab6372c9aa3cce7dd85
dcarmona8 <dcarmonamd@gmail.com>
2023-07-25 00:09:20 +0200

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+ default = "rg-recursos_generales"
+}
+
+variable "public_ip" {
+ default = "rg-recursos_ip"
+}
+
+variable "location_name" {
+  default = "uksouth" 
+}
+
+variable "network_name" {
+  default = "vnet1"
+}
+
+variable "subnet_name" {
+  default = "subnet1"
+}
